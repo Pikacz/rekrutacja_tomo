9fe065ebcde018d5872cd62205630a7b9d7b278c
Paweł Czerwiński <pawel.czerwinski@proexe.pl>
2026-03-14 19:37:47 +0100

    FIXME: 4

--- BuggyBright-RecruitmentTask/RickyBuggyBright/Sources/Common/AppearanceFrequency.swift
+++ BuggyBright-RecruitmentTask/RickyBuggyBright/Sources/Common/AppearanceFrequency.swift
@@ -7,27 +7,27 @@ import Foundation
 
 
 /// Level selected based on number of appearances in the show, if character appeared 10 times or more - it's high, if 3 times or more - its medium, if 1 or lower - it's low
-enum AppearanceFrequency: Int {
-    case high = 10
-    case medium = 3
-    case low = 1
-}
-
-// FIXME: 4 - Fix issue with initialisation not working accordingly to requirements written above, try improving clean code approach
-
-extension AppearanceFrequency {
+struct AppearanceFrequency {
+    enum FrequencyType {
+        case high, medium, low
+    }
+    
+    let count: Int
+    let type: FrequencyType
+    
     init(count: Int) {
-        if count >= 1 {
-            self = .low
+        self.count = count
+        type = if count >= 10 {
+            .high
         } else if count >= 3 {
-            self = .medium
+            .medium
         } else {
-            self = .high
+            .low
         }
     }
     
     var popularity: String {
-        switch self {
+        switch type {
         case .high:
             return "So popular!"
         case .medium:
